642e293e0c5ee038312264a611bba352ec19311b
yjhoo98 <yjhoo98@gmail.com>
2025-02-11 10:48:09 +0900

    v0.6 day02 prime number

--- day02.py
+++ day02.py
@@ -1,6 +1,16 @@
+n=int(input("Input Number:"))
+count=0
+for i in range(1,n+1):
+    if n %i==0:
+        count=count+1
+if count==2:
+    print(f"{n} is prime number")
+else:
+    print(f"{n} is not prime number")
+
 # dan =int(input("Input dan:"))
 # for i in range(1,10):
 #     print(f"{dan}*{i}={dan*i}")
-for dan in range(2,10):
-    for i in range(1,10):
-        print(f"{dan}*{i}={dan*i}")
+# for dan in range(2,10):
+#     for i in range(1,10):
+#         print(f"{dan}*{i}={dan*i}")
