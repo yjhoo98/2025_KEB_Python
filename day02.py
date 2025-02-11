b791fb4981e301e283b30d5cbbeddf1bf12f7950
yjhoo98 <yjhoo98@gmail.com>
2025-02-11 16:17:00 +0900

    v1.0

--- day02.py
+++ day02.py
@@ -1,20 +1,32 @@
+#1) for-> while
+#2) while로 구간 소수를 출력
+#3) **대신 pow함수
 def is_prime(num)->bool:
     """
     if num is prime number->return True/if num is not prime number->return False
     :param num:integer number
     :return:boolean type
     """
-
     if num >= 2:
-
-        for i in range(2, int(num ** 0.5) + 1):
+        i=2
+        while i<=int(num**0.5):
             if num % i == 0:
-                # is_prime = False
-                #break
+
                 return False
-            # print(i, end=' ')
+            else:
+                i=i+1
     else:
         return False
+    # if num >= 2:
+    #
+    #     for i in range(2, int(num ** 0.5) + 1):
+    #         if num % i == 0:
+    #             # is_prime = False
+    #             #break
+    #             return False
+    #         # print(i, end=' ')
+    # else:
+    #     return False
     return True
 n=int(input("Input Number:"))
 
