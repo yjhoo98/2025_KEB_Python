1053e14d8ceae47256037556b4c16273b1fc8e1a
yjhoo98 <yjhoo98@gmail.com>
2025-02-11 13:31:40 +0900

    v0.9 day02 prime number

--- day02.py
+++ day02.py
@@ -1,24 +1,25 @@
+def is_prime(num)->bool:
+    """
+    if num is prime number->return True/if num is not prime number->return False
+    :param num:integer number
+    :return:boolean type
+    """
 
+    if num >= 2:
+
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                # is_prime = False
+                #break
+                return False
+            # print(i, end=' ')
+    else:
+        return False
+    return True
 n=int(input("Input Number:"))
-is_prime=True
-if n>=2:
-    # count=0
-    for i in range(2,int(n**0.5)+1):
-        if n %i==0:
-            # count=count+1
-            is_prime=False
-            break
-        print(i,end=' ')
-else:
-    is_prime=False
-if is_prime:
+
+if is_prime(n):
     print(f"{n} is prime number")
 else:
     print(f"{n} is not prime number")
 
-# dan =int(input("Input dan:"))
-# for i in range(1,10):
-#     print(f"{dan}*{i}={dan*i}")
-# for dan in range(2,10):
-#     for i in range(1,10):
-#         print(f"{dan}*{i}={dan*i}")
