d6d53abf046267c99209808f816b846540bd0cc3
yjhoo98 <yjhoo98@gmail.com>
2025-02-11 10:44:08 +0900

    v0.5 day02

--- day02.py
+++ day02.py
@@ -1,6 +1,6 @@
-dan =int(input("Input dan:"))
-for i in range(1,10,1):
-    print(f"{dan}*{i}={dan*i}")
-# for dan in range(2,10,1):
-#     for i in range(1,10,1):
-#         print(f"{dan}*{i}={dan*i}")
+# dan =int(input("Input dan:"))
+# for i in range(1,10):
+#     print(f"{dan}*{i}={dan*i}")
+for dan in range(2,10):
+    for i in range(1,10):
+        print(f"{dan}*{i}={dan*i}")
