30cb8586cbd514a2f36340ea5fc348dcd068831f
yjhoo98 <yjhoo98@gmail.com>
2025-02-12 18:26:12 +0900

    v2.6

--- day03.py
+++ day03.py
@@ -5,15 +5,31 @@ def print_menu(num):
 
 drinks=['위스키','와인','소주','고량주']
 drinks_foods=['초콜릿','치즈','삼겹살','양꼬치']
-new_menu=input("주류와 안주 입력:").split(' ')
-drinks.append(new_menu[0])
-drinks_foods.append(new_menu[1])
-drinks.append('사케')
-drinks_foods.append('광어회')
-drinks.append('위스키')
-drinks_foods.append('낙곱새')
-drinks.pop(0)
-drinks_foods.pop(0)
+
+while True:
+    ask = input(f'1)메뉴 추가 2)메뉴 삭제 3)변경없음:')
+    if int(ask)==1:
+        new_menu = input("주류와 안주 입력:").split(' ')
+        if new_menu[0] not in drinks:
+            print('이미 있는 메뉴')
+            continue
+        else:
+            drinks.append(new_menu[0])
+            drinks_foods.append(new_menu[1])
+    elif int(ask)==2:
+        del_menu = input("주류와 안주 입력:").split(' ')
+        if del_menu[0] not in drinks:
+            print('존재하지 않는 메뉴')
+            continue
+        else:
+            drinks.remove(del_menu[0])
+            drinks_foods.remove(del_menu[1])
+
+    elif int(ask)==3:
+        print(f'변경없음.')
+        break
+print(drinks)
+
 
 menu_list='다음 술중에 고르세요.\n'
 for i in range(len(drinks_foods)):
