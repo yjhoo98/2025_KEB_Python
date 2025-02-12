819a514efa2d1e4df0574377c5ef239d78ab2067
yjhoo98 <yjhoo98@gmail.com>
2025-02-12 17:29:30 +0900

    v2.2

--- day03.py
+++ day03.py
@@ -1,3 +1,4 @@
+#메뉴 삭제 추가에 대응되는 코드 추가
 import random
 
 drinks_foods=[['위스키','초콜릿'],['와인','치즈'],['소주','삼겹살'],['고량주','양꼬치']]
@@ -6,15 +7,19 @@ drinks_foods.append(["사케", "광어회"])
 drinks_foods.append([ "위스키", "낙곱새"])
 
 drinks_foods.pop(0)
+menu_list='다음 술중에 고르세요.\n'
+for i in range(len(drinks_foods)):
+    menu_list=menu_list+f'{i+1}){drinks_foods[i][0]} '
+menu_list=menu_list+f'{len(drinks_foods)+1}) 아무거나 {len(drinks_foods)+2}) 종료:'
 
 while True:
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods[0][0]}   2) {drinks_foods[1][0]}   3) {drinks_foods[2][0]}   4) {drinks_foods[3][0]}   5) {drinks_foods[4][0]}   6) 아무거나   7) 종료 : ')
-    if menu in{'1','2','3','4','5'}:
+    menu=input(menu_list)
+    if int(menu)<=len(drinks_foods):
         a=int(menu)-1
         print(f'{drinks_foods[a][0]}에 어울리는 안주는 {drinks_foods[a][1]} 입니다')
-    elif menu == '6':
+    elif int(menu)==len(drinks_foods)+1:
         num=random.randint(0,len(drinks_foods)-1)
         print(f'{drinks_foods[num][0]}에 어울리는 안주는 {drinks_foods[num][1]} 입니다')
-    elif menu == '7':
+    elif int(menu)==len(drinks_foods)+2:
         print(f'다음에 또 오세요')
         break
