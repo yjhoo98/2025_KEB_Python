c6248e3e5c47e46aa11a4bbef46f562ae256faaf
yjhoo98 <yjhoo98@gmail.com>
2025-02-12 17:49:02 +0900

    v2.5

--- day03.py
+++ day03.py
@@ -2,17 +2,19 @@
 import random
 def print_menu(num):
     print(f'{drinks[num]}에 어울리는 안주는 {drinks_foods[num]} 입니다')
-# drinks_foods=[['위스키','초콜릿'],['와인','치즈'],['소주','삼겹살'],['고량주','양꼬치']]
+
 drinks=['위스키','와인','소주','고량주']
 drinks_foods=['초콜릿','치즈','삼겹살','양꼬치']
-# drinks_foods.append(["사케", "광어회"])
-# drinks_foods.append([ "위스키", "낙곱새"])
+new_menu=input("주류와 안주 입력:").split(' ')
+drinks.append(new_menu[0])
+drinks_foods.append(new_menu[1])
 drinks.append('사케')
 drinks_foods.append('광어회')
 drinks.append('위스키')
 drinks_foods.append('낙곱새')
 drinks.pop(0)
 drinks_foods.pop(0)
+
 menu_list='다음 술중에 고르세요.\n'
 for i in range(len(drinks_foods)):
     menu_list=menu_list+f'{i+1}){drinks[i]} '
