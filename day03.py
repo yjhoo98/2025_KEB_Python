cc7ce552292ce7248616f43816f14a4e094c5a3d
yjhoo98 <yjhoo98@gmail.com>
2025-02-12 16:48:13 +0900

    v2.1

--- day03.py
+++ day03.py
@@ -1,25 +1,13 @@
 import random
-# drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
-drinks_foods=[['위스키','초콜릿'],['와인','치즈'],['소주','삼겹살'],['고량주','양꼬치']]
-# print(drinks_foods)
-# print(drinks_foods.pop("고량주"))
-# print(drinks_foods)
 
-#del drinks_foods["위스키"]
-#drinks_foods["사케"] = "광어회"
+drinks_foods=[['위스키','초콜릿'],['와인','치즈'],['소주','삼겹살'],['고량주','양꼬치']]
+
 japan_drinks_foods = ["사케", "광어회"]
 drinks_foods.append(japan_drinks_foods)
 drinks_foods.append([ "위스키", "낙곱새"])
-# print(drinks_foods)
-#drink = input(drinks_foods.keys())
-# drinks_foods_keys = list(drinks_foods)
-# print(drinks_foods_keys)
-# #print(drinks_foods_keys.pop(0))
-# print(drinks_foods_keys.remove("위스키"))
-# print(drinks_foods_keys)
-#print(random.choice(drinks_foods_keys))
+
 drinks_foods.pop(0)
-print(drinks_foods[4][0])
+
 while True:
     menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods[0][0]}   2) {drinks_foods[1][0]}   3) {drinks_foods[2][0]}   4) {drinks_foods[3][0]}   5) {drinks_foods[4][0]}   6) 아무거나   7) 종료 : ')
     if menu == '1':
