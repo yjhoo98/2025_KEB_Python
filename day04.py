de782edab69ad9d6add028d303c66e684385ca69
yjhoo98 <yjhoo98@gmail.com>
2025-02-13 11:04:46 +0900

    v2.9 day04 Kiosk

--- day04.py
+++ day04.py
@@ -4,15 +4,15 @@ def print_menu_total_price(num):
     global total_price
     print(f'{drinks[num]}에 어울리는 안주는 {drinks_foods[num]} 입니다.')
     print(f'가격은 {prices[num]}입니다')
+    amounts[num]=amounts[num]+1
     total_price = total_price+prices[num]
 # d_s_p={"위스키":['초콜릿',50_000]}
 # print(d_s_p["위스키"][1])
 drinks=['위스키','와인','소주','고량주']
 drinks_foods=['초콜릿','치즈','삼겹살','양꼬치']
 prices=[50000,30000,5000,7500]
-amounts=list()
-for i in range(len(drinks)):
-    amounts.append(0)
+amounts=[0 for i in range(len(drinks))]
+
 while True:
     ask = input(f'1)메뉴 추가 2)메뉴 삭제 3)변경없음:')
     if int(ask)==1:
@@ -38,8 +38,6 @@ while True:
     elif int(ask)==3:
         print(f'변경없음.')
         break
-print(drinks)
-
 
 menu_list='다음 술중에 고르세요.\n'
 for i in range(len(drinks_foods)):
@@ -57,6 +55,9 @@ while True:
         print_menu_total_price(rnum)
 
     elif int(menu)==len(drinks)+2:
-        print(f'가격은 {total_price}원 입니다.')
         print(f'다음에 또 오세요')
         break
+for k in range(len(drinks)):
+    if amounts[k]!=0:
+        print(f"주류명:{drinks[k]} 수량:{amounts[k]} 단가:{prices[k]} 소계:{prices[k]*amounts[k]}")
+print(f"총 금액:{total_price}")
