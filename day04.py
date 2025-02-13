63a43bf017a49fdeecf3b274267202dbe9e0a9b8
yjhoo98 <yjhoo98@gmail.com>
2025-02-13 17:09:47 +0900

    v3.9

--- day04.py
+++ day04.py
@@ -1,5 +1,5 @@
 #이중 decorator 적용/성능측정/discription/factorial
-import time
+
 def log_decorator(func):
     def wrapper(*args,**kwargs):
         print(f"Function Name:{func.__name__}")
@@ -19,8 +19,8 @@ def descript_func(func):
         return r
     return inner_desc
 @log_decorator
-def greet(name,greeting="안녕하세요",age=0):
-    return f"{greeting},{name}"
+def greet(name,greeting="안녕하세요",age=None):
+    return f"{greeting},{name},(age{age})"if age else f"{greeting},{name}"
 print(greet("인하"))
 print(greet("인상","안녕"))
 print(greet("James","HELLO"))
