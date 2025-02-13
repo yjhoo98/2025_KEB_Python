206c6f892f2c7631440951207eddddea6d317d71
yjhoo98 <yjhoo98@gmail.com>
2025-02-13 16:18:59 +0900

    v3.5 double decorator

--- day04.py
+++ day04.py
@@ -1,7 +1,19 @@
-#SOLID
-#Open cloesd principle:개방 페쇄 원칙(확장에는 열려 있고 수정에는 닫혀있는 원칙)
+#이중 decorator 적용/성능측정/discription/factorial
 import time
+def discript_func(func):
+    def inner_disc(*args):
+        print(func.__name__)
+        print(func.__doc__)
+        r = func(*args)
+        return r
+    return inner_disc
+
 def time_decorator(func):
+    """
+
+    :param func:function
+    :return:time
+    """
     def wrapper(*arg):
         s=time.time()
         r=func(*arg)
@@ -9,8 +21,15 @@ def time_decorator(func):
         print(f"{e-s}초 소요")
         return r
     return wrapper
-#@time_decorator
+
+@time_decorator
+@discript_func
 def factorial_repetition(n)->int:
+    """
+
+    :param n:integer number
+    :return:n!
+    """
     result=1
     for i in range(2,n+1):
         result=result*i
@@ -20,7 +39,4 @@ number =int(input())
 print(f"{number}!={factorial_repetition(number)}")
 # e=time.time()
 # print(e-s)
-number=int(input())
-f1=time_decorator(factorial_repetition)
-print(f'{number}!={factorial_repetition(number)}')
-print(f'{number}!={f1(number)}')
+
