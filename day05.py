496f189c1d2df3ce9455411874604da719fbbd20
yjhoo98 <yjhoo98@gmail.com>
2025-02-14 17:14:59 +0900

    v4.4

--- day05.py
+++ day05.py
@@ -1,1 +1,4 @@
 import yjhmath
+
+print(yjhmath.my_abs(-99))
+print(yjhmath.my_fibonacci_recursion(6))
--- yjhmath.py
+++ yjhmath.py
@@ -7,3 +7,15 @@ def my_abs(n)->int:
     if n<0:
         return-n
     return n
+def my_fibonacci_recursion(n):
+    """
+    fibonacci sequence using recurion
+    :param n:integer
+    :return:fibonacci sequence
+    """
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return my_fibonacci_recursion(n-1)+my_fibonacci_recursion(n-2)
